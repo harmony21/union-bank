b1cd4195e7d9f966d82750142f291e887fcf67d8
Inna Botvinnik <21icelady97@gmail.com>
2018-04-12 11:34:04 +1000

    the first full version 1920

---
+++ js/mask.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+    $("#phone").mask("+7 (999) 999-99-99");
+});
+jQuery(function($){
+    $("#phone1").mask("+7 (999) 999-99-99");
+});
+jQuery(function($){
+    $("#phone2").mask("+7 (999) 999-99-99");
+});
